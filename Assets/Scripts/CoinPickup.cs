c7db963d1c2c2812c955693899af7d70646d6a46
king-prawns <giorgio.gamberoni@gmail.com>
2023-12-19 15:28:19 +0000

    coin

--- Assets/Scripts/CoinPickup.cs
+++ Assets/Scripts/CoinPickup.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class CoinPickup : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    void OnTriggerEnter2D(Collider2D other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
+        if (other.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
         
     }
 }
